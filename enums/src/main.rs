aa9fc33e9bf1064f4c7f2d20278baffe8d9a0a66
vijay kiran samrath <vijaysamrath462@gmail.com>
2024-10-30 21:13:19 +0530

    enum If let

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,78 +1,85 @@
+// // #[derive(Debug)]
+
+// // enum IpAddrKind {
+// //     V4,
+// //     V6,
+// // }
+
+// // struct IpAdress {
+// //     address: String,
+// //     kind: IpAddrKind,
+// // }
+
+// // fn main() {
+
+// //     let google_address = IpAdress {
+// //         address: String::from("1.2.3.4"),
+// //         kind: IpAddrKind::V4,
+// //     };
+// //     route(google_address);
+// // }
+
+// // fn route(ip: IpAdress) {
+// //     println!("Routing request to IP {} of kind {:?}", ip.address, ip.kind);
+
+// // }
+
+// // enum Message {
+// //     Quit,
+// //     Move { x: i32, y: i32 },
+// //     Write(String),
+// //     ChangeColor(i32, i32, i32),
+// // }
+
+// // fn main() {
+// //     // if we insert boolean it will become bool
+// //     // let op = Some(true);
+
+// //     // if we insert string it will become string
+// //     let op = Some(String::from("abc"));
+
+// //     // let op: Option<i8> = Some(5);
+
+
+// // }
+
 // #[derive(Debug)]
-
-// enum IpAddrKind {
-//     V4,
-//     V6,
+// enum UsState{
+//     Alabama,
+//     Alaska,
 // }
 
-// struct IpAdress {
-//     address: String,
-//     kind: IpAddrKind,
-// }
-
-// fn main() {
-
-//     let google_address = IpAdress {
-//         address: String::from("1.2.3.4"),
-//         kind: IpAddrKind::V4,
-//     };
-//     route(google_address);
-// }
-
-// fn route(ip: IpAdress) {
-//     println!("Routing request to IP {} of kind {:?}", ip.address, ip.kind);
-
-// }
-
-// enum Message {
-//     Quit,
-//     Move { x: i32, y: i32 },
-//     Write(String),
-//     ChangeColor(i32, i32, i32),
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
 // }
 
 // fn main() {
-//     // if we insert boolean it will become bool
-//     // let op = Some(true);
-
-//     // if we insert string it will become string
-//     let op = Some(String::from("abc"));
-
-//     // let op: Option<i8> = Some(5);
-
-
+//     let coin = Coin::Quarter(UsState::Alaska);
+//     println!("Value is {}", match_in_cent(coin));
 // }
 
-#[derive(Debug)]
-enum UsState{
-    Alabama,
-    Alaska,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
+// fn match_in_cent(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(UsState::Alaska) => {
+//             println!("Hello from Alaska");
+//             2
+//         }
+//         Coin::Quarter(state) =>{
+//             println!("Got Q of value {:?}", state);
+//             25
+//         }
+//     }
+// }
 
 fn main() {
-    let coin = Coin::Quarter(UsState::Alaska);
-    println!("Value is {}", match_in_cent(coin));
-}
-
-fn match_in_cent(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(UsState::Alaska) => {
-            println!("Hello from Alaska");
-            2
-        }
-        Coin::Quarter(state) =>{
-            println!("Got Q of value {:?}", state);
-            25
-        }
+    let config_max = Some(3_u8);
+    if let Some(max) = config_max{
+        println!("THe maximum is configured to be {max}");
     }
 }
